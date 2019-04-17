36ac469ce077a059daa837c2e909c15e62e47f0c
benny <35829331+yanx27@users.noreply.github.com>
2019-04-17 11:11:18 +0800

    pointnet2

--- model/pointnet2.py
+++ model/pointnet2.py
@@ -131,7 +131,7 @@ class PointNet2PartSeg_msg_one_hot(nn.Module):
 class PointNet2SemSeg(nn.Module):
     def __init__(self, num_classes):
         super(PointNet2SemSeg, self).__init__()
-        self.sa1 = PointNetSetAbstraction(1024, 0.1, 32, 3, [32, 32, 64], False)
+        self.sa1 = PointNetSetAbstraction(1024, 0.1, 32, 6 + 3, [32, 32, 64], False)
         self.sa2 = PointNetSetAbstraction(256, 0.2, 32, 64 + 3, [64, 64, 128], False)
         self.sa3 = PointNetSetAbstraction(64, 0.4, 32, 128 + 3, [128, 128, 256], False)
         self.sa4 = PointNetSetAbstraction(16, 0.8, 32, 256 + 3, [256, 256, 512], False)
@@ -144,8 +144,8 @@ class PointNet2SemSeg(nn.Module):
         self.drop1 = nn.Dropout(0.5)
         self.conv2 = nn.Conv1d(128, num_classes, 1)
 
-    def forward(self, xyz):
-        l1_xyz, l1_points = self.sa1(xyz, None)
+    def forward(self, xyz,points):
+        l1_xyz, l1_points = self.sa1(xyz, points)
         l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
         l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
         l4_xyz, l4_points = self.sa4(l3_xyz, l3_points)
@@ -158,6 +158,7 @@ class PointNet2SemSeg(nn.Module):
         x = self.drop1(F.relu(self.bn1(self.conv1(l0_points))))
         x = self.conv2(x)
         x = F.log_softmax(x, dim=1)
+        x = x.permute(0, 2, 1)
         return x
 
 
