e365b9f7b9c3d7d6444278d92e298e3f078794e1
Benny <775410794@qq.com>
2021-06-25 13:12:03 +0800

    Revert "Merge pull request #113 from yushiangw/master"
    This reverts commit 768cd018b73c5e358f7783fec32140f6a687b133, reversing
    changes made to ba4c5cf3a1e2b735d696c59a43c38345c3d003b9.

--- models/pointnet2_utils.py
+++ models/pointnet2_utils.py
@@ -16,17 +16,28 @@ def pc_normalize(pc):
     pc = pc / m
     return pc
 
-
 def square_distance(src, dst):
     """
-     Input:
-         src: source points, [B, N, C]
-         dst: target points, [B, M, C]
-    Output:
-          dist: per-point square distance, [B, N, M]
-    """ 
-    return torch.cdist(src, dst)
+    Calculate Euclid distance between each two points.
 
+    src^T * dst = xn * xm + yn * ym + zn * zm；
+    sum(src^2, dim=-1) = xn*xn + yn*yn + zn*zn;
+    sum(dst^2, dim=-1) = xm*xm + ym*ym + zm*zm;
+    dist = (xn - xm)^2 + (yn - ym)^2 + (zn - zm)^2
+         = sum(src**2,dim=-1)+sum(dst**2,dim=-1)-2*src^T*dst
+
+    Input:
+        src: source points, [B, N, C]
+        dst: target points, [B, M, C]
+    Output:
+        dist: per-point square distance, [B, N, M]
+    """
+    B, N, _ = src.shape
+    _, M, _ = dst.shape
+    dist = -2 * torch.matmul(src, dst.permute(0, 2, 1))
+    dist += torch.sum(src ** 2, -1).view(B, N, 1)
+    dist += torch.sum(dst ** 2, -1).view(B, 1, M)
+    return dist
 
 
 def index_points(points, idx):
@@ -83,30 +94,16 @@ def query_ball_point(radius, nsample, xyz, new_xyz):
     Return:
         group_idx: grouped points index, [B, S, nsample]
     """
-    device  = xyz.device
+    device = xyz.device
     B, N, C = xyz.shape
     _, S, _ = new_xyz.shape
-    
     group_idx = torch.arange(N, dtype=torch.long).to(device).view(1, 1, N).repeat([B, S, 1])
     sqrdists = square_distance(new_xyz, xyz)
-    
-    # B, S
-    min_idx=torch.min(sqrdists,dim=-1)[1]
-    min_idx=min_idx.unsqueeze(-1) 
-    # B, S, N
-    group_first = min_idx.repeat([1, 1, nsample])
-
     group_idx[sqrdists > radius ** 2] = N
-
-    # B,S,N to B,S,n
-    group_idx = group_idx.sort(dim=-1)[0][:, :, :nsample] 
-
+    group_idx = group_idx.sort(dim=-1)[0][:, :, :nsample]
+    group_first = group_idx[:, :, 0].view(B, S, 1).repeat([1, 1, nsample])
     mask = group_idx == N
     group_idx[mask] = group_first[mask]
-
-    if group_idx.max()> N-1:
-        raise Exception('invalid indices')
-
     return group_idx
 
 
